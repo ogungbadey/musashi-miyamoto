a863b8795d813e06969eeff4599b5f6eb3a61a6e
ogungbadey <tok2ogunz@gmail.com>
2022-05-03 22:01:12 +0100

    some hacky fix, that hopefully works

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import { ThemeProvider } from "./context/ThemeContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <ThemeProvider>
+    <BrowserRouter >
+      <ThemeProvider basename={process.env.PUBLIC_URL}>
         <App />
       </ThemeProvider>
-    </HashRouter>
+    </BrowserRouter >
   </React.StrictMode>
 );
 
